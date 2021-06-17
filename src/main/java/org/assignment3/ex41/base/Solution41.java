7cefaedb35a55ca590e16fe1893d0dd323a2faf3
Akwill47 <wil.weat@hotmail.com>
2021-06-16 23:07:17 -0400

    ye

--- src/main/java/org/assignment3/ex41/base/Solution41.java
+++ src/main/java/org/assignment3/ex41/base/Solution41.java
@@ -1,4 +1,8 @@
 package org.assignment3.ex41.base;
-
+import java.util.Scanner;
 public class Solution41 {
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+
+    }
 }
